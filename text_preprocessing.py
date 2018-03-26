b48057bdc379dcde007bc61e79c45ca21a44a5c7
Bao Anh <bao-anh.nguyen@etu.univ-lyon1.fr>
2018-03-26 12:59:07 +0200

    Caution: rename of function and class, add docstring
    Some function and global variable are rename to be more exact.
    A start of docstring (in restructuredtext format) but need a better writting

--- text_preprocessing.py
+++ text_preprocessing.py
@@ -6,14 +6,14 @@ import pandas
 
 
 
-file_in = 'collecting_file.json'
-file_out = 'preprocess_tweet_file.json'
-file_out_csv = 'preprocess_tweet_file.csv'
+tweet_file = 'collecting_file.json'
+preprocess_tweet_json = 'preprocess_tweet_file.json'
+preprocess_tweet_csv = 'preprocess_tweet_file.csv'
 
 
 #Cleaning function may leave blankspace at the start or the end because it only remove emot
 # ex : ":) hello :)" => " hello "
-class Tweet_process:
+class Tweet_preprocess:
 
 
     #à mettre des condition avec isinstance pour gerer cas chaine de tweet ou json etc
@@ -22,14 +22,15 @@ class Tweet_process:
         self.text = ''
         self.clean_text = ''
         self.emoticons_list = []
-        self.polarity = 0 # 1 : happy, 0: neutral, -1 : angry
+        self.polarity = 0 # 1 : positive, 0: neutral, -1 : negative
         self.words_list = []
 
-
-    #construct the class from tweet data
-    #line : json line
-    #return: boolean -> sucess construct or not
     def constructor_json(self, line):
+        """
+        **construct the class from tweet data**
+        :param line: json
+        :return: boolean
+        """
         tweet_data = json.loads(line)
 
         # dont know how to handle probleme tweet dont have key text
@@ -41,7 +42,6 @@ class Tweet_process:
             tweet_text_exist = True
         else:
             tweet_text_exist = False
-            # tweet_text = ""
 
 
         if tweet_text_exist:
@@ -57,8 +57,6 @@ class Tweet_process:
                     process_reliable = True
                 else:
                     process_reliable = False
-            # except UnknownLanguage:
-            #     print ("Can't tokenize, can't detect language of tweet")
             except:
                 raise
 
@@ -69,20 +67,23 @@ class Tweet_process:
 
             #cleaning
             self.clean_store_emoticon(tweet_text) #normally clean_text empty, this one fill it
-            self.clean_url(self.clean_text)
-            self.clean_hashtag(self.clean_text)
-            self.clean_mention(self.clean_text)
+            self.clean_text = preprocessor.clean(self.clean_text)
 
-            self.polarity_emoticon(self.emoticons_list)
+            self.polarity = self.polarity_emoticon(self.emoticons_list)
 
             self.tokenize_text(self.clean_text)
             return True
         else:
             return False
 
-    #remove emoji using unicode then store in emoticons_list
-    #sentence: string
+
     def clean_store_emoticon(self, sentence):
+        """
+        **remove emoji using unicode then store in emoticons_list**
+        :param sentence: string
+        :return: string: text clean
+        """
+
         #regex
         emoticons_unicode = r"""
                             ([\U0001F300-\U0001F64F])| #emoticons + Uncategorized
@@ -102,72 +103,64 @@ class Tweet_process:
 
         self.clean_text = line
 
-    #polarity work for sentence with only 1 type of emoji (happy/angry) else neutral
-    #store result in polarity
-    #emoticon_list: list of unicode emoji
     def polarity_emoticon(self, emoticon_list):
+        """
+        **polarity work for sentence with only 1 type of emoji (positive/negative) else neutral**
+        :param emoticon_list: list of unicode emoji
+        :return: 1 (positive), -1 (negative), 0 (neutral)
+        """
+
         #regex
-        happy_emote_unicode = r"""
+        positive_emote_unicode = r"""
                                 ([\U0001F600-\U0001F607])|
                                 ([\U0001F609-\U0001F60F])|
                                 ([\U0001F617-\U0001F61D])
                                 """
-        angry_emote_unicode = r"""
+        negative_emote_unicode = r"""
                                 ([\U0001F61E-\U0001F629])|
                                 ([\U0001F630-\U0001F631])
                                 """
 
-        re_happy_emote = re.compile(happy_emote_unicode, re.VERBOSE|re.UNICODE)
-        re_agnry_emote = re.compile(angry_emote_unicode, re.VERBOSE|re.UNICODE)
+        re_positive_emote = re.compile(positive_emote_unicode, re.VERBOSE|re.UNICODE)
+        re_agnry_emote = re.compile(negative_emote_unicode, re.VERBOSE|re.UNICODE)
 
-        counter_happy_emote = 0
-        counter_angry_emote = 0
+        counter_positive_emote = 0
+        counter_negative_emote = 0
 
         #count number of emoji in text
         for x in emoticon_list:
-            if re_happy_emote.match(x):
-                counter_happy_emote += 1
+            if re_positive_emote.match(x):
+                counter_positive_emote += 1
             elif re_agnry_emote.match(x):
-                counter_angry_emote += 1
+                counter_negative_emote += 1
 
         #processing polarity
-        if counter_happy_emote > 0 and counter_angry_emote > 0:
-            self.polarity = 0
-        elif counter_happy_emote > 0:
-            self.polarity = 1
-        elif counter_angry_emote > 0:
-            self.polarity = -1
+        if counter_positive_emote > 0 and counter_negative_emote > 0:
+            return 0
+        elif counter_positive_emote > 0:
+            return 1
+        elif counter_negative_emote > 0:
+            return -1
         else:
-            self.polarity = 0 #traitement non nécessaire du au constructeur
+            return 0
 
-    #tokenize text in to words then store in clean_text
-    #text: string
     def tokenize_text(self, text):
+        """
+        tokenize text in to words then store in clean_text
+        :param text: string
+        :return: None
+        """
         text_token = preprocessor.tokenize(text)
         self.words_list = text_token.split()
 
-    #clean url of text then store in clean_text
-    #text: string
-    def clean_url(self, text):
-        preprocessor.set_options(preprocessor.OPT.URL)
-        self.clean_text = preprocessor.clean(text)
-
-    #clean mention of text then store in clean_text
-    #text: string
-    def clean_mention(self, text):
-        preprocessor.set_options(preprocessor.OPT.MENTION)
-        self.clean_text = preprocessor.clean(text)
-
-    #clean hashtag of text then store in clean_text
-    #text: string
-    def clean_hashtag(self, text):
-        preprocessor.set_options(preprocessor.OPT.HASHTAG)
-        self.clean_text = preprocessor.clean(text)
 
 
-    #probleme avec language ou dans constructeur c'est = null donc aime pas (je crois)
-    #return: A json of the class with all att of class
     def create_json(self):
+        """
+        **Create a json of Tweet_preprocess class
+        :return: A json of the class with all att of class
+        """
+
         #json format
         data = {"language": "","text": "", "clean_text": "", "emoticons_list" : [], "polarity" : "0", "words_list" : []}
 
@@ -181,20 +174,28 @@ class Tweet_process:
 
         return data
 
-#extract data from json file to add to tab with Tweet_process
-#tab: a list of Tweet_process or empty
-#file_name: path to a json file[IN]
-def fill_tab_Tweet_process_from_json_file(tab, file_name):
+
+def fill_tab_Tweet_preprocess_from_json_file(tab, file_name):
+    """
+    **Extract data from json file to add to tab with Tweet_preprocess**
+    :param tab: a list of Tweet_preprocess or empty
+    :param file_name: path to a json file[IN]
+    :return: None
+    """
     with open (file_name) as file:
         for line in file:
-            x = Tweet_process()
+            x = Tweet_preprocess()
             if x.constructor_json(line): #construct and test in same time
                 tab.append(x)
 
-#extract data of tab of Tweet_process to write it on a file in json format
-#tab: a list of Tweet_process
-#file_name: path to a json file[OUT]
-def fill_json_file_from_tab_Tweet_process(tab, file_name, neutral_polarity = False):
+def fill_json_file_from_tab_Tweet_preprocess(tab, file_name, neutral_polarity = False):
+    """
+    **extract data of tab of Tweet_preprocess to write it on a file in json format**
+    :param tab: a list of Tweet_preprocess
+    :param file_name: path to a json file[OUT]
+    :param neutral_polarity: boolean : True keep neutral tweet else dont
+    :return: None
+    """
     with open(file_name, 'w') as file:
         for tweet_preprocess in tab:
             if neutral_polarity == False:
@@ -210,12 +211,14 @@ def fill_json_file_from_tab_Tweet_process(tab, file_name, neutral_polarity = Fal
 #upgrade possible: take a json file (dict methode)
 #with row name parameter (can chose what to write in csv)
 #and the test on polarity != 0 can be done in json
-
-#write clean_text and polarity of a table of Tweet_process in a csv file
-#tab : a list of Tweet_process
-#file_name: path to a csv file [OUT]
-#json_key:
-def fill_csv_file_from_Tweet_process(tab, file_out, neutral_polarity = False):
+def fill_csv_file_from_Tweet_preprocess(tab, file_out, neutral_polarity = False):
+    """
+    #Write clean_text and polarity of a table of Tweet_preprocess in a csv file
+    :param tab: a list of Tweet_preprocess
+    :param file_out: path to a csv file [OUT]
+    :param neutral_polarity: boolean : True keep neutral tweet else dont
+    :return: None
+    """
     df_result = pandas.DataFrame()
     for tweet_preprocess in tab:
         if neutral_polarity == False:
@@ -230,13 +233,16 @@ def fill_csv_file_from_Tweet_process(tab, file_out, neutral_polarity = False):
     df_result.to_csv(file_out)
 
 
-#convert dataFrame from a csv file to a numpy array
-#csv_file_in: path to a csv file [IN]
-#row/column = single label, array label, slice label, boolean array
-#   by default: function use all row and column clean_text,polarity
-#return: a numpy Array
 def csv_file_to_numpy_array(csv_file_in, row=slice(0,None, None), column=["clean_text", "polarity"]):
-    df = pandas.read_csv(file_out_csv)
+    """
+    **convert dataFrame from a csv file to a numpy array**
+    by default: function use all row and column clean_text,polarity
+    :param csv_file_in:
+    :param row: single label, array label, slice label, boolean array
+    :param column: single label, array label, slice label, boolean array
+    :return: numpy Array
+    """
+    df = pandas.read_csv(preprocess_tweet_csv)
     sub_df = df.loc[row,column]
     return sub_df.values
 
@@ -244,29 +250,29 @@ def csv_file_to_numpy_array(csv_file_in, row=slice(0,None, None), column=["clean
 #main
 
 tab_tweet = []
-fill_tab_Tweet_process_from_json_file(tab_tweet, file_in)
-fill_json_file_from_tab_Tweet_process(tab_tweet, file_out, True)
-fill_csv_file_from_Tweet_process(tab_tweet, file_out_csv, True)
-numpy_array = csv_file_to_numpy_array(file_out_csv)
+fill_tab_Tweet_preprocess_from_json_file(tab_tweet, tweet_file)
+fill_json_file_from_tab_Tweet_preprocess(tab_tweet, preprocess_tweet_json, True)
+fill_csv_file_from_Tweet_preprocess(tab_tweet, preprocess_tweet_csv, True)
+numpy_array = csv_file_to_numpy_array(preprocess_tweet_csv)
+
 
 # Partie test
 # print ("debut partie test")
+# with open(tweet_file) as file:
+#     for line in file:
+#         print ("============================= TWEET ========================")
+#         tweet = json.loads(line)
+#         print (tweet['id'])
 
-# ligne = slice(0,None,None)
-# df = pandas.read_csv(file_out_csv)
-# df2 = df.set_index("clean_text", drop = False)
-# sub_df = df.loc[ligne , ["clean_text", "polarity"]]
-# print (sub_df.values)
-# numpyArr = csv_file_to_numpy_array(file_out_csv)
-# print (numpy_array)
+# print(numpy_array)
 # print ("fin partie test")
 
 # for i in range (10):
 #     print ("tweet numero ", i)
-#     # print (tab_tweet[i].create_json())
-#     print ("text complet")
-#     print (tab_tweet[i].text)
-#     print ("text clean")
-#     print (tab_tweet[i].clean_text)
-#     print ("emoticon list")
-#     print (tab_tweet[i].emoticons_list)
+    # print (tab_tweet[i].create_json())
+    # print ("text complet")
+    # print (tab_tweet[i].text)
+    # print ("text clean")
+    # print (tab_tweet[i].clean_text)
+    # print ("emoticon list")
+    # print (tab_tweet[i].emoticons_list)
